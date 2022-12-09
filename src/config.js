3958dc2e55211ff2037c5985f2ed16d417b639d6
Eduardo Felipe Donadio de Almeida Serra <efelipe10@hotmail.com>
2022-12-09 17:57:05 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'c46a884e-d772-486d-a0ea-0dbdef380daa';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://admin:admin@cluster0.yoklq9n.mongodb.net/ndstr?retryWrites=true&w=majority',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodestrdud;AccountKey=tq44LMMPbD3d8NyXwuWgsPjHEpVQnUFO31aV6yJTf2MW6zJNpx4N8Jq8kbSGVYdFbaiKrzcEErmF+ASt97ZmqQ==;EndpointSuffix=core.windows.net'
+    connectionString: '',
+    sendgridKey: '',
+    containerConnectionString: ''
 }
